19f85e60e0a3b17b8b70877844b1eafce6620a33
sajai20 <110714149+sajai20@users.noreply.github.com>
2022-11-23 18:49:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,14 +9,26 @@ function init() {
     textEl.value = null;
 }
 function red() {
+    if (paraEl.classList.contains('green'))
+        paraEl.classList.remove('green');
+    if (paraEl.classList.contains('blue'))
+        paraEl.classList.remove('blue');
     paraEl.classList.add('red')
     paraEl.innerHTML = `<h4>${textEl.value}</h4>`;
 }
 function green() {
+    if (paraEl.classList.contains('red'))
+        paraEl.classList.remove('red');
+    if (paraEl.classList.contains('blue'))
+        paraEl.classList.remove('blue');
     paraEl.classList.add('green')
     paraEl.innerHTML = `<h4>${textEl.value}</h4>`;
 }
 function blue() {
+    if (paraEl.classList.contains('green'))
+        paraEl.classList.remove('green');
+    if (paraEl.classList.contains('red'))
+        paraEl.classList.remove('red');
     paraEl.classList.add('blue')
     paraEl.innerHTML = `<h4>${textEl.value}</h4>`;
 }
